dd67f2f8b49e3e09cf7b9e0f88b5e05d9f37c709
Fleisar <me@fle.su>
2022-04-24 13:22:59 +0700

    connect menu buttons interactions

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -2,6 +2,7 @@ import { Packet } from './packet';
 import Pages, { PageDOM } from './classes/pages';
 import Windows, { WindowDOM } from './classes/windows';
 import './enchantments';
+import './menu/interactions';
 import Config from './config';
 
 Packet.link();
