f84d79dbfcb0956930e867c00a6216fc03bafb6d
Fleisar <me@fle.su>
2022-04-24 13:22:29 +0700

    create player page

--- src/assets/js/plugins/core/index.ts
+++ src/assets/js/plugins/core/index.ts
@@ -3,6 +3,7 @@ import { importHTML } from './utils/import';
 import { ContainerHandler } from '../../objects/containers';
 import { ObjectStore } from '../../objects/shared';
 import General from './pages/general';
+import Player from './pages/player';
 
 Packet.link();
 
@@ -17,19 +18,26 @@ export default class Core implements ContainerHandler {
 
 	constructor() {
 		this.subHandlers.general = new General(this);
+		this.subHandlers.player = new Player(this);
 	}
 
-	handler(root): boolean {
+	handler(root, data): boolean {
 		switch (root) {
+			case 'player':
+				this.subHandlers.player.open(root, data);
+				break;
 			case 'general':
 			default:
-				this.subHandlers.general.open();
+				this.subHandlers.general.open(root, data);
 		}
 		return true;
 	}
 
 	close(root): boolean {
 		switch (root) {
+			case 'player':
+				this.subHandlers.player.close();
+				break;
 			case 'general':
 			default:
 				this.subHandlers.general.close();
@@ -39,11 +47,12 @@ export default class Core implements ContainerHandler {
 
 	async setup(): Promise<boolean> {
 		await this.subHandlers.general.setup();
+		await this.subHandlers.player.setup();
 		Packet.store.Pages.open('core.general');
 		return true;
 	}
 
-	roots = ['general'];
+	roots = ['general', 'player'];
 
 }
 
